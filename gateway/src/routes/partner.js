d3fef21be4555f577c862085a8955ffb263ea664
John Higgins <johnhbenetech@users.noreply.github.com>
2021-04-15 15:02:38 -0500

    Update partner.js

--- gateway/src/routes/partner.js
+++ gateway/src/routes/partner.js
@@ -151,7 +151,7 @@ router.post('/partner/submit', async (req, res) => {
               const mathsharePayload = {
                 editCode,
                 shareCode,
-                metadata: metadataFinal,
+                metadataFinal,
               };
               payload = getPayload(config, mathsharePayload);
               authHeaders = await getRequestHeaders(config, metadataFinal);
